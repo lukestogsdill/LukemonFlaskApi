75d5ef70f5b6760f09060985353c89e5bd444911
BuildTools <unconfigured@null.spigotmc.org>
2023-06-15 15:47:18 -0500

    heroku

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ from datetime import timedelta
 
 # CONFIG SECTION
 class Config():
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
